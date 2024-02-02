b3aa52ad041229608585cb70fac7150194ce472e
mahesh dash <maheshdash84821@gmail.com>
2024-02-02 23:52:48 +0530

    Api Response Handle

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,10 @@
-class ApiResponse{
-    constructor(statusCode, data,message = "Sucess"){
-        this.statusCode = statusCode
-        this.data = data
-        this.message= message
-        this.success = statusCode < 400
-    }
+class ApiResponse {
+  constructor(statusCode, data, message = "Sucess") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
 }
+
+export default ApiResponse;
