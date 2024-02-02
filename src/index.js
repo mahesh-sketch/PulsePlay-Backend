4e9df18c9cf10c53589808b284d122e462705fac
mahesh dash <maheshdash84821@gmail.com>
2024-02-02 20:54:47 +0530

    Src File

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connecttin Failed !!", err);
+  });
+
+
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constant";
+import express from "express";
+
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/
+    ${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening at port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error:", error);
+    throw error;
+  }
+})();*/
---
+++ src/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose, { Mongoose, Schema } from "mongoose";
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, //for searching purpose
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password){
+  return await bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema);
