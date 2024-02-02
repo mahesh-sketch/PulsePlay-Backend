f0c18c3f175d282fbdc0b97441397b673aac4d58
mahesh dash <maheshdash84821@gmail.com>
2024-02-02 23:54:55 +0530

    All middleware function userd here

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
