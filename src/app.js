d8f8471c3df78917339df2b849d6aacb5e021d36
mahesh dash <maheshdash84821@gmail.com>
2024-02-03 23:34:36 +0530

    user router added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -14,5 +15,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//For user router
+app.use("/api/v1/user", userRouter);
 
 export { app };
