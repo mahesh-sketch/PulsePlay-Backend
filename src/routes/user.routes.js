79d0052935c38af840bdd62a97ca2e7c87781048
mahesh dash <maheshdash84821@gmail.com>
2024-02-04 22:55:37 +0530

    Login and logout route added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js";
+import verifyJwt from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -18,5 +19,8 @@ userRouter.route("/register").post(
   userController.registerUser
 );
 userRouter.route("/login").post(userController.loginUser);
+
+//secured routes
+userRouter.route("/logout").post(verifyJwt, userController.logoutUser);
 
 export default userRouter;
