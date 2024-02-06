21f80e8d77c5058c831791b62590d5aa1526a254
mahesh dash <maheshdash84821@gmail.com>
2024-02-06 20:23:16 +0530

    Router added of many user operation

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,4 +23,16 @@ userRouter.route("/login").post(userController.loginUser);
 //secured routes
 userRouter.route("/logout").post(verifyJwt, userController.logoutUser);
 userRouter.route("/refresh-token").post(userController.refreshAccessToken);
+userRouter
+  .route("/change-password")
+  .post(verifyJwt, upload.none(), userController.changeCurrentPassword);
+userRouter
+  .route("/get-current-user")
+  .get(verifyJwt, userController.getCurrentUser);
+userRouter
+  .route("/update-account-details")
+  .post(verifyJwt, upload.none(), userController.updateAccountDetails);
+userRouter
+  .route("/update-user-avatar")
+  .post(verifyJwt,upload.single("avatar"),userController.updateUserAvatar);
 export default userRouter;
