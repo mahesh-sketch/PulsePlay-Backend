7f1efdd4c6cc80b0685f34086bee2454ef91a0fe
mahesh dash <maheshdash84821@gmail.com>
2024-02-08 13:33:00 +0530

    add more userroutes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,16 +23,37 @@ userRouter.route("/login").post(userController.loginUser);
 //secured routes
 userRouter.route("/logout").post(verifyJwt, userController.logoutUser);
 userRouter.route("/refresh-token").post(userController.refreshAccessToken);
+
 userRouter
   .route("/change-password")
   .post(verifyJwt, upload.none(), userController.changeCurrentPassword);
+
 userRouter
   .route("/get-current-user")
   .get(verifyJwt, userController.getCurrentUser);
+
 userRouter
   .route("/update-account-details")
   .post(verifyJwt, upload.none(), userController.updateAccountDetails);
+
 userRouter
   .route("/update-user-avatar")
-  .post(verifyJwt,upload.single("avatar"),userController.updateUserAvatar);
+  .patch(verifyJwt, upload.single("avatar"), userController.updateUserAvatar);
+
+userRouter
+  .route("/update-cover-image")
+  .patch(
+    verifyJwt,
+    upload.single("coverImage"),
+    userController.updateUserCoverImage
+  );
+
+userRouter
+  .route("/c/:username")
+  .get(verifyJwt, userController.getUserChannelProfile);
+
+userRouter
+  .route("/watch-history")
+  .get(verifyJwt, userController.getWatchHistory);
+
 export default userRouter;
