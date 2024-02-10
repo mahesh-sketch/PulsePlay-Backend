994f7158b6203fe51c67eabfe59762ba1b620580
mahesh dash <maheshdash84821@gmail.com>
2024-02-10 20:25:11 +0530

    updated user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import { User } from "../models/user.model.js";
+import User from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import cloudinaryMethod from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
