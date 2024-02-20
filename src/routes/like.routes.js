34171814593e2ef7c1c39bd06def808e4353ff78
mahesh dash <maheshdash84821@gmail.com>
2024-02-20 21:36:55 +0530

    Get Like By Video Id added

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import likeController from "../controllers/like.controller.js";
-import  verifyJWT  from "../middlewares/auth.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
@@ -9,5 +9,6 @@ router.route("/toggle/v/:videoId").post(likeController.toggleVideoLike);
 router.route("/toggle/c/:commentId").post(likeController.toggleCommentLike);
 router.route("/toggle/t/:tweetId").post(likeController.toggleTweetLike);
 router.route("/videos").get(likeController.getLikedVideos);
+router.route("/videos/:videoId").get(likeController.getLikedByVideoId);
 
 export default router;
