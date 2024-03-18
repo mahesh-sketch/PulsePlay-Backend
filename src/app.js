42915a5b9aad83e8ca51da2d20c243b116b26c91
mahesh dash <maheshdash84821@gmail.com>
2024-03-18 20:50:24 +0530

    payload size increased

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "32kb" }));
+app.use(express.urlencoded({ extended: true, limit: "32kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
